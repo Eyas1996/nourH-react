d61203cad0149d42c84b0a5d63222192adcfa80b
Eyas1996 <eyas.hm7@gmail.com>
2023-03-30 02:15:54 +0200

    add style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <h1>Test</h1>
+      <h1>Design is my life</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./style.css"
+
 import App from "./components/App";
 
 // Render Method takes to parapemter (what, where)
