7bdcb958c9d53b377667fa7322f0944afa06efdc
Eyas1996 <eyas.hm7@gmail.com>
2023-03-31 17:17:51 +0200

    Upload Files

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,27 +6,35 @@ function Social() {
   return (
     <ul className="list-group">
       <SocialItem
-        media="Facebook"
-        id="facebook"
-        img="https://picsum.photos/60"
+        p={{
+          media: "Facebook",
+          id: "facebook",
+          img: "https://picsum.photos/60",
+        }}
       />
 
       <SocialItem
-        media="Youtube"
-        id="youtube"
-        img="https://picsum.photos/61"
+        p={{
+          media: "Youtube",
+          id: "youtube",
+          img: "https://picsum.photos/61",
+        }}
       />
 
       <SocialItem
-        media="Instagram"
-        id="instagram"
-        img="https://picsum.photos/62"
+        p={{
+          media: "Instagram",
+          id: "instagram",
+          img: "https://picsum.photos/62",
+        }}
       />
 
       <SocialItem
-        media="LinkedIn"
-        id="linkedin"
-        img="https://picsum.photos/63"
+        p={{
+          media: "LinkedIn",
+          id: "linkedin",
+          img: "https://picsum.photos/63",
+        }}
       />
     </ul>
   );
--- src/components/SocialItem.js
+++ src/components/SocialItem.js
@@ -3,10 +3,10 @@ import React from "react";
 const SocialItem = (props) => {
   return (
     <li className="list-group-item">
-      <img src={props.img} alt={`Icon of ${props.media}`} />
-      <input className="form-check-input" type="checkbox" id={props.id} />
-      <label className="form-check-label" htmlFor={props.id}>
-        {props.media}
+      <img src={props.p.img} alt={`Icon of ${props.p.media}`} />
+      <input className="form-check-input" type="checkbox" id={props.p.id} />
+      <label className="form-check-label" htmlFor={props.p.id}>
+        {props.p.media}
       </label>
     </li>
   );
