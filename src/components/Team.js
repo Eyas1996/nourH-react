114e6d0a8db651ae43b98c22890bade196afa399
Eyas1996 <eyas.hm7@gmail.com>
2023-04-02 00:19:10 +0200

    Upload Files

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,83 +2,73 @@ import React from "react";
 
 import TeamMember from "./TeamMember";
 function Team() {
-  return (
-    <div className="container">
-      <TeamMember
-        info={{
-          img: "/images/01.jpg",
-          name: "Jinx",
-          title: "webdesigner",
-          phone: "+49 123 8956 9943",
-          email: "JinxJinx@gmail.com",
-          website: "arcane.com",
-        }}
-      />
+  const info = [
+    {
+      id: 1,
+      img: "/images/01.jpg",
+      name: "Jinx",
+      title: "webdesigner",
+      email: "JinxJinx@gmail.com",
+      website: "arcane.com",
+    },
+    {
+      id: 2,
+      img: "/images/02.jpg",
+      name: "Vi",
+      title: "Web developer",
+      phone: "+49 123 8956 9943",
+      email: "ViVi@gmail.com",
+      website: "arcane.com",
+    },
+    {
+      id: 3,
+      img: "/images/03.jpg",
+      name: "Vander",
+      title: "Cloud engineer",
+      phone: "+49 123 8956 9943",
+      email: "Vander@gmail.com",
+      website: "arcane.com",
+    },
+    {
+      id: 4,
+      img: "/images/04.png",
+      name: "Silco",
+      title: "Network administrator",
+      email: "Silco@gmail.com",
+      website: "arcane.com",
+    },
+    {
+      id: 5,
+      img: "/images/05.png",
+      name: "Jayce",
+      title: "Data scientist",
+      phone: "+49 123 8956 9943",
+      email: "Jayce@gmail.com",
+      website: "arcane.com",
+    },
+    {
+      id: 6,
+      img: "/images/06.jpg",
+      name: "Viktor",
+      title: "Software engineer",
+      phone: "+49 123 8956 9943",
+      email: "Viktor@gmail.com",
+    },
+  ];
 
-      <TeamMember
-        info={{
-          img: "/images/02.jpg",
-          name: "Vi",
-          title: "Data scientist",
-          phone: "+49 123 8956 9943",
-          email: "ViVi@gmail.com",
-          website: "arcane.com",
-        }}
-        />
-      <TeamMember
-        info={{
-          img: "/images/03.jpg",
-          name: "Vander",
-          title: "Cloud engineer",
-          phone: "+49 123 8956 9943",
-          email: "Vander@gmail.com",
-          website: "arcane.com",
-        }}
+  const members = info.map((member) => (
+    <TeamMember
+      key={member.id}
+      img={member.img}
+      name={member.name}
+      title={member.title}
+      phone={member.phone}
+      email={member.email}
+      website={member.website}
+    />
+  ));
 
-        /*
-        Network administrator
-        Software engineer
-         Web developer
-         webdesigner
-         Data scientist
-         Cloud engineer
-        */
-      />
-      
-      <TeamMember
-        info={{
-          img: "/images/04.png",
-          name: "Vi",
-          title: "Data scientist",
-          phone: "+49 123 8956 9943",
-          email: "ViVi@gmail.com",
-          website: "arcane.com",
-        }}
-        />
-        
-      <TeamMember
-        info={{
-          img: "/images/05.png",
-          name: "Vi",
-          title: "Data scientist",
-          phone: "+49 123 8956 9943",
-          email: "ViVi@gmail.com",
-          website: "arcane.com",
-        }}
-        />
-        
-      <TeamMember
-        info={{
-          img: "/images/06.jpg",
-          name: "Vi",
-          title: "Data scientist",
-          phone: "+49 123 8956 9943",
-          email: "ViVi@gmail.com",
-          website: "arcane.com",
-        }}
-        />
-    </div>
-  );
+  return <div className="container">{members}</div>;
 }
 
 export default Team;
--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -4,16 +4,28 @@ const TeamMember = (props) => {
   // props.p.name ? console.log("he") : console.log("neeee");
 
   return (
-    <div className="card" >
+    <div className="card">
       <div className="image">
-        <img src={props.info.img} alt="" />
+        <img src={props.img} alt="" />
       </div>
-      <div className="info">
-        <h1 className="name">{props.info.name }</h1>
-        <span className="title">{props.info.title}</span>
-        <span className="phone">Phone: {props.info.phone}</span>
-        <span className="email">Email: {props.info.email}</span>
-        <span className="website">Website: {props.info.website ? props.info.website : props.info.website}</span>
+      <div
+        className="info"
+        style={{
+          backgroundColor: props.website ? "black" : "white",
+          color: props.website ? "white" : "black",
+        }}
+      >
+        <h1 className="name">{props.name}</h1>
+        <span className="title">{props.title}</span>
+        <span className="phone">
+          Phone: {props.phone ? props.phone : "There is no Phone"}
+        </span>
+        <span className="email">
+          Email: {props.email ? props.email : "There is no Email"}
+        </span>
+        <span className="website">
+          Website: {props.website ? props.website : "There is no website"}
+        </span>
       </div>
     </div>
   );
